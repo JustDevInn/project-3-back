e1ec52f04371f4ff1b2a923c2da24185135b545b
Justin Peeters <Peeters.justin@yahoo.com>
2020-04-16 17:56:02 +0200

    newcommityay

--- models/wods.js
+++ models/wods.js
@@ -4,7 +4,10 @@ const Schema = mongoose.Schema;
 // create schema and model >>TO DO>> connect to Wods folder
 const wodSchema = new Schema({
     name: String,
-    description: String
+    wod: String,
+    description: String,
+    tag: String
+
 });
 
 const Wod = mongoose.model('wods', wodSchema);
--- routes/wod.js
+++ routes/wod.js
@@ -7,22 +7,15 @@ const Wod = require("../models/wods")
 
 
 router.get("/wods", (req, res) => {
-        Wod
-            .find()
-            .then((data) => {
-                res.json(data)
-            })
-            .catch((err) => {
-                res.send("error");
-            })
-    })
-    // router.get('/', function(req, res, next) {
-    //     res.json([
-    //         { id: 1, username: "userJsFromApi" },
-    //         { id: 2, username: "hardcoded" }
-    //     ])
-
-// });
+    Wod
+        .find()
+        .then((data) => {
+            res.json(data)
+        })
+        .catch((err) => {
+            res.send("error");
+        })
+})
 
 
 module.exports = router;
